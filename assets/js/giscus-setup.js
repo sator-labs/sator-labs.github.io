36c731cb37f77c17b3f1c8937b9e6ec1975327fe
sator-labs <129434630+sator-labs@users.noreply.github.com>
2026-02-04 19:32:45 +0000

    Deploying to gh-pages from @ sator-labs/sator-labs.github.io@70aa54ab94fdcd8fe3e6d1b8c5efaf8b10494357 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "sator-labs/gator-labs.github.io",
+    "data-repo": "sator-labs/sator-labs.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
